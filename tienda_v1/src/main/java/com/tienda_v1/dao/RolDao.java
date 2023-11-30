13021be827a51e5f1f319c96551fcf8f74032770
valverdeS7 <valverdetete17@gmail.com>
2023-11-29 22:48:22 -0600

    semana 11

---
+++ tienda_v1/src/main/java/com/tienda_v1/dao/RolDao.java
@@ -0,0 +1,8 @@
+package com.tienda_v1.dao;
+
+import com.tienda_v1.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
---
+++ tienda_v1/src/main/java/com/tienda_v1/service/CorreoService.java
@@ -0,0 +1,13 @@
+package com.tienda_v1.service;
+
+import jakarta.mail.MessagingException;
+
+public interface CorreoService {
+    public void enviarCorreoHtml(
+            String para, 
+            String asunto, 
+            String contenidoHtml) 
+            throws MessagingException;
+}
+
+
